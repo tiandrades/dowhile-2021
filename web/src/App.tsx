9039b9f8f1db14113ed535dbc739bb844d711a4e
Tiago Andrade <68028173+TiagooAndrade@users.noreply.github.com>
2021-10-22 12:20:53 -0400

    ✨ feat: create application website

---
+++ web/src/App.tsx
@@ -0,0 +1,20 @@
+import { useContext } from 'react';
+
+import { LoginBox } from './components/LoginBox';
+import { MessageList } from './components/MessageList';
+import { SendMessageForm } from './components/SendMessageForm';
+
+import { AuthContext } from './context/auth';
+
+import styles from './App.module.scss';
+
+export function App() {
+  const { user } = useContext(AuthContext);
+
+  return (
+    <main className={`${styles.contentWrapper} ${!!user ? styles.contentSigned : ''}`}>
+      <MessageList />
+      {!!user ? <SendMessageForm /> : <LoginBox />}
+    </main>
+  )
+}
